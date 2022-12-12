325f241f80741e346362599402155c25b09e29d5
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-12 19:18:21 +0000

    cookie

--- js/script.js
+++ js/script.js
@@ -16,6 +16,6 @@ if (navigator.serviceWorker) {
 /**
  * this function
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function myCookieClicked() {
+  
 }
